d5518a379d4696ad97b89999026066160a4bdffa
Elena Slyusareva <sportgirl55@bk.ru>
2020-11-11 18:21:46 +0500

    Layout is almost ready

--- src/js/script.js
+++ src/js/script.js
@@ -2,20 +2,44 @@
 
 $('.radio-item').on('click', function() {
   $(this).addClass('check').siblings().removeClass('check');
+
 })
+
+
+$('input:checkbox').click(function(){
+	if ($(this).is(':checked')) {
+		$(this).parent().addClass('check');
+	} else {
+		$(this).parent().removeClass('check');
+	}
+});
+
+
+
+
+$('input:checkbox').click(function(){
+	if ($(this).is(':checked')){
+		$('#agree').removeAttr('disabled');
+	} else {
+		$('#agree').attr('disabled', 'disabled'); 
+	}
+});
+
+
+
+
 
 const buttons = Array.from(document.querySelectorAll('button'));
 const consent = document.querySelector('.consent');
 const main = document.querySelector('#main');
 
+
 for (let btn of buttons) {
   btn.addEventListener('click', (e) => {
-    console.log('ПРивет');
-    if (!e.currentTarget.closest('.consent')) {
-    e.currentTarget.closest('.form-item').classList.add('hidden');
-    e.currentTarget.closest('.form-item').nextElementSibling.classList.remove('hidden');
+    if (e.currentTarget.closest('section') != main.lastElementChild) {
+      e.currentTarget.closest('.form-item').classList.add('hidden');
+      e.currentTarget.closest('.form-item').nextElementSibling.classList.remove('hidden');
     let nextSibChildren = e.currentTarget.closest('.form-item').parentElement.nextElementSibling.children;
-    
       for (let child of nextSibChildren) {
         if (child.hasAttribute('data-state')) {  
           child.getAttribute('data-state') == "active" ? child.classList.remove('hidden') : null;
@@ -23,13 +47,49 @@ for (let btn of buttons) {
         }
       }
     } else {
-    for (let item of document.querySelector('#main').children)
-      !item.classList.contains('consent') ? item.classList.add('hidden') : null;
-      consent.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
-      consent.querySelector('.form-item[data-state="active"]').classList.add('hidden');
+     for (let item of document.querySelector('#main').children)
+      item != main.lastElementChild ? item.classList.add('hidden') : null;
+      main.lastElementChild.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
+      main.lastElementChild.querySelector('.form-item[data-state="active"]').classList.add('hidden');
+
     }
   });
 }
+
+
+
+
+// Кнопки ПРОДОЛЖИТЬ. Рабочий вариант
+
+// const buttons = Array.from(document.querySelectorAll('button'));
+// const consent = document.querySelector('.consent');
+// const main = document.querySelector('#main');
+
+// for (let btn of buttons) {
+//   btn.addEventListener('click', (e) => {
+ 
+//     if (!e.currentTarget.closest('.consent')) {
+//     e.currentTarget.closest('.form-item').classList.add('hidden');
+//     e.currentTarget.closest('.form-item').nextElementSibling.classList.remove('hidden');
+//     let nextSibChildren = e.currentTarget.closest('.form-item').parentElement.nextElementSibling.children;
+    
+//       for (let child of nextSibChildren) {
+//         if (child.hasAttribute('data-state')) {  
+//           child.getAttribute('data-state') == "active" ? child.classList.remove('hidden') : null;
+//           child.getAttribute('data-state') == "hidden" ? child.classList.add('hidden') : null;
+//         }
+//       }
+//     } else {
+//     for (let item of document.querySelector('#main').children)
+//       !item.classList.contains('consent') ? item.classList.add('hidden') : null;
+//       consent.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
+//       consent.querySelector('.form-item[data-state="active"]').classList.add('hidden');
+//     }
+//   });
+// }
+
+
+
 
 // const changeBtn = Array.from(document.querySelectorAll('.change'));
 // for (let change of changeBtn) {
@@ -50,6 +110,10 @@ for (let btn of buttons) {
 // }
 
 
+
+// Кнопка ИЗМЕНИТЬ рабочая только в правильной очередности
+
+
 const changeBtn = Array.from(document.querySelectorAll('.change'));
 for (let change of changeBtn) {
   change.addEventListener('click', (e) => {
@@ -58,61 +122,9 @@ for (let change of changeBtn) {
     if (e.currentTarget.closest('.choose')) {
       e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="active"]').classList.add('hidden');
       e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
-      
-// main.children.forEach(elem => {
-//   elem.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-//   elem.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
-//   elem.querySelector('.form-item[data-state="checked"]').classList.add('hidden');
-// });
-
-
-
-      
-      // e.currentTarget.closest('section').nextElementSibling.nextElementSibling.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-      // e.currentTarget.closest('section').nextElementSibling.nextElementSibling.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
-      // e.currentTarget.closest('section').nextElementSibling.nextElementSibling.querySelector('.form-item[data-state="checked"]').classList.add('hidden');
     } else {
     e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="active"]').classList.add('hidden');
     e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
    }
-
-   
- 
- 
   })
 }
-
-
-
-// main.children.each(function () {
-//   $('.form-item[data-state="active"]').addClass('hidden');
-//   $('.form-item[data-state="hidden"]').removeClass('hidden');
-//   $('.form-item[data-state="checked"]').addClass('hidden');
-// })
-
-
-// $('.form-item:has(this)')
-// $('p:has(b)')
-
-// $('button').on('click', function () {
-//   $('.form-item:has(button)').addClass('hidden');
-//   $('.form-item:has(button)').nextSiblingElement('.form-item').removeClass('hidden');
-//   // $(this).parent().addClass('hidden');
-//   // $(this).parent().addClass('hidden');
-// })
-    
-
-
-
-  
-// const changeBtn = Array.from(document.querySelectorAll('.change'));
-// for (let change of changeBtn) {
-//   change.addEventListener('click', (e) => {
-//     e.currentTarget.closest('.form-item').classList.add('hidden');
-//     e.currentTarget.closest('section').querySelector('.form-item[data-state="active"]').classList.remove('hidden');
-//     e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-//     e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
- 
-    
-//   })
-// }
