fc2ae3fd87314b85adae69f039bd2ef180b4828d
Elena Slyusareva <sportgirl55@bk.ru>
2020-11-13 03:02:15 +0500

    Almost ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
 'use strict';
 
+// переключени радиокнопок
+
 $('.radio-item').on('click', function() {
   $(this).addClass('check').siblings().removeClass('check');
 
 })
+
+// переключение чекбоксов
 
 
 $('input:checkbox').click(function(){
@@ -15,116 +19,111 @@ $('input:checkbox').click(function(){
 });
 
 
-
+// Проверка чекбоксов на заполненность
+// Работает только для одного чекбокса, а нужно для двух
 
 $('input:checkbox').click(function(){
 	if ($(this).is(':checked')){
 		$('#agree').removeAttr('disabled');
 	} else {
-		$('#agree').attr('disabled', 'disabled'); 
-	}
+    $('#agree').attr('disabled', 'disabled'); 
+ 	}
 });
 
 
-
-
+// Лучший рабочий вариант
+// Переключение секций
 
 const buttons = Array.from(document.querySelectorAll('button'));
-const consent = document.querySelector('.consent');
 const main = document.querySelector('#main');
 
 
 for (let btn of buttons) {
   btn.addEventListener('click', (e) => {
-    if (e.currentTarget.closest('section') != main.lastElementChild) {
-      e.currentTarget.closest('.form-item').classList.add('hidden');
-      e.currentTarget.closest('.form-item').nextElementSibling.classList.remove('hidden');
-    let nextSibChildren = e.currentTarget.closest('.form-item').parentElement.nextElementSibling.children;
-      for (let child of nextSibChildren) {
-        if (child.hasAttribute('data-state')) {  
-          child.getAttribute('data-state') == "active" ? child.classList.remove('hidden') : null;
-          child.getAttribute('data-state') == "hidden" ? child.classList.add('hidden') : null;
+   
+      if (e.currentTarget.closest('section') != main.lastElementChild) {
+        e.currentTarget.closest('.form-item').classList.add('hidden');
+        e.currentTarget.closest('.form-item').nextElementSibling.classList.remove('hidden');
+      let nextSibChildren = e.currentTarget.closest('.form-item').parentElement.nextElementSibling.children;
+        for (let child of nextSibChildren) {
+          if (child.hasAttribute('data-state')) {  
+            child.getAttribute('data-state') == "active" ? child.classList.remove('hidden') : null;
+            child.getAttribute('data-state') == "hidden" ? child.classList.add('hidden') : null;
+          }
         }
+      } else {
+       for (let item of document.querySelector('#main').children)
+        item != main.lastElementChild ? item.classList.add('hidden') : null;
+        main.lastElementChild.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
+        main.lastElementChild.querySelector('.form-item[data-state="active"]').classList.add('hidden');
+  
       }
-    } else {
-     for (let item of document.querySelector('#main').children)
-      item != main.lastElementChild ? item.classList.add('hidden') : null;
-      main.lastElementChild.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
-      main.lastElementChild.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-
-    }
+   
   });
 }
 
 
-
-
-// Кнопки ПРОДОЛЖИТЬ. Рабочий вариант
-
-// const buttons = Array.from(document.querySelectorAll('button'));
-// const consent = document.querySelector('.consent');
-// const main = document.querySelector('#main');
-
-// for (let btn of buttons) {
-//   btn.addEventListener('click', (e) => {
- 
-//     if (!e.currentTarget.closest('.consent')) {
-//     e.currentTarget.closest('.form-item').classList.add('hidden');
-//     e.currentTarget.closest('.form-item').nextElementSibling.classList.remove('hidden');
-//     let nextSibChildren = e.currentTarget.closest('.form-item').parentElement.nextElementSibling.children;
-    
-//       for (let child of nextSibChildren) {
-//         if (child.hasAttribute('data-state')) {  
-//           child.getAttribute('data-state') == "active" ? child.classList.remove('hidden') : null;
-//           child.getAttribute('data-state') == "hidden" ? child.classList.add('hidden') : null;
-//         }
-//       }
-//     } else {
-//     for (let item of document.querySelector('#main').children)
-//       !item.classList.contains('consent') ? item.classList.add('hidden') : null;
-//       consent.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
-//       consent.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-//     }
-//   });
-// }
-
-
-
-
-// const changeBtn = Array.from(document.querySelectorAll('.change'));
-// for (let change of changeBtn) {
-//   change.addEventListener('click', (e) => {
-//     e.currentTarget.closest('.form-item').classList.add('hidden');
-//     e.currentTarget.closest('section').querySelector('.form-item[data-state="active"]').classList.remove('hidden');
-//     for (let items of Array.from(document.getElementsByClassName('.form-item'))) {
-//       items.getAttribute('data-state') == "active" ? items.classList.add('hidden') : null;
-//       items.getAttribute('data-state') == "hidden" ? items.classList.remove('hidden') : null;
-  
-//     }
-    
-//     // items.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-//     // items.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
- 
-    
-//   })
-// }
-
-
-
-// Кнопка ИЗМЕНИТЬ рабочая только в правильной очередности
-
+// Кнопки "Изменить"
 
 const changeBtn = Array.from(document.querySelectorAll('.change'));
 for (let change of changeBtn) {
   change.addEventListener('click', (e) => {
+    document.querySelectorAll('.form-item[data-state="active"]').forEach(function (elem) {
+      elem.classList.add('hidden');
+    })
+    document.querySelectorAll('.form-item[data-state="checked"]').forEach(function (elem) {
+      elem.classList.add('hidden');
+    })
+    document.querySelectorAll('.form-item[data-state="hidden"]').forEach(function (elem) {
+      elem.classList.remove('hidden');
+    })
+
     e.currentTarget.closest('.form-item').classList.add('hidden');
     e.currentTarget.closest('section').querySelector('.form-item[data-state="active"]').classList.remove('hidden');
-    if (e.currentTarget.closest('.choose')) {
-      e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-      e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
-    } else {
-    e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="active"]').classList.add('hidden');
-    e.currentTarget.closest('section').nextElementSibling.querySelector('.form-item[data-state="hidden"]').classList.remove('hidden');
-   }
+    if (e.currentTarget.closest('section').previousElementSibling != null) {
+      e.currentTarget.closest('section').querySelector('.form-item[data-state="hidden"]').classList.add('hidden');
+      e.currentTarget.closest('section').previousElementSibling.querySelector('.form-item[data-state="checked"]').classList.remove('hidden');
+    } 
   })
 }
+
+
+
+
+
+  // $('#check-1').on('change', function(){
+  //   if($(this).is(':checked')){
+  //     $('button').prop('disabled', false);
+  //   } else {
+  //     $('button').prop('disabled', true);
+  //   }
+  // });
+
+  // $('#check-2').on('change', function(){
+  //   if($(this).is(':checked')){
+  //     $('button').prop('disabled', false);
+  //   } else {
+  //     $('button').prop('disabled', true);
+  //   }
+  // });
+
+
+
+
+
+  // document.getElementById('check-1').addEventListener('click', function (e) {
+  //   document.getElementById('agree').disabled = !e.target.checked
+  // })
+  // document.getElementById('check-2').addEventListener('click', function (e) {
+  //   document.getElementById('agree').disabled = !e.target.checked
+  // })
+
+// let checkboxes = document.getElementsByClassName('check-item');
+// document.getElementById('agree').addEventListener ('click', function () {
+//    if (document.getElementById('check-1').checked === true) {
+
+   
+//       this.removeAttribute("disabled");
+//     }  
+//   })
+
